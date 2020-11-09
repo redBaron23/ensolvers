158ed6a1aa3dd34e629f324545c0b19a8f27e73b
redbaron23 <pato>
2020-11-09 17:32:42 -0300

    Read from dynamodb

--- client/src/components/ToDoList.jsx
+++ client/src/components/ToDoList.jsx
@@ -1,7 +1,12 @@
-import { React, useState } from "react";
+import axios from "axios";
+
+import { api } from "../config";
+
+import { React, useState, useEffect } from "react";
 import ToDoItem from "./ToDoItem";
 import NewBar from "./NewBar";
-import { Box, Button, Grid, makeStyles,Typography } from "@material-ui/core/";
+import { Box, Button, Grid, makeStyles, Typography } from "@material-ui/core/";
+import { test } from "../fun/helper.js";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -9,10 +14,26 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const getTasks = folderName => {
+  let res;
+  res = axios.get(api + "/items", {
+    params: {
+      folderName: folderName
+    }
+  });
+  console.log("Me devolvio", res);
+  return res;
+};
 const ToDoList = () => {
   const classes = useStyles();
 
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    if (!items.length) {
+      getTasks("folderName").then(i => setItems(i.data.map(i => i.name)));
+    }
+  });
 
   const destroy = (item, e) => {
     const newItems = items.filter(i => i !== item);
@@ -22,26 +43,25 @@ const ToDoList = () => {
   const createItem = item => {
     const exist = items.filter(i => i === item);
 
-    
     if (!exist.length) {
-      let arr = [...items,item]
+      let arr = [...items, item];
       setItems([...items, item]);
     }
   };
   return (
     <div className={classes.root}>
       <Box border={1}>
-	<Typography variant="h3">To-Do List</Typography>
-      <Grid container xs={12} sm={12} md={12} spacing={2}>
-        {items.map(i => (
-          <Grid key={i} item xs={12} sm={6} md={3}>
-            <ToDoItem key={i} text={i} destroy={e => destroy(i, e)} />
+        <Typography variant="h3">To-Do List</Typography>
+        <Grid container xs={12} sm={12} md={12} spacing={2}>
+          {items.map(i => (
+            <Grid key={i} item xs={12} sm={6} md={3}>
+              <ToDoItem key={i} text={i} destroy={e => destroy(i, e)} />
+            </Grid>
+          ))}
+          <Grid item xs={12}>
+            <NewBar create={createItem} type="Task" />
           </Grid>
-        ))}
-        <Grid item xs={12}>
-          <NewBar create={createItem} type="Task" />
         </Grid>
-      </Grid>
       </Box>
     </div>
   );
