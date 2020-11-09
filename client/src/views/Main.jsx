f7c254eb40937b8361486279d25d01f6f665b43f
redbaron23 <pato>
2020-11-09 18:57:35 -0300

    amplify

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -1,22 +1,14 @@
-import logo from "../logo.svg";
 import "../App.css";
-import ToDoItem from "../components/ToDoItem"
+import ToDoList from "../components/ToDoList"
+
+import FolderList from "../components/FolderList"
+
+
 const Main = () => {
   return (
     <header className="App-header">
-      <ToDoItem />
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      <ToDoList />
+      <FolderList/>
     </header>
   );
 }
