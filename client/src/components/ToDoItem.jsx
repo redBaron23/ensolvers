7136601c8978b3e36cccf05e21382f58bc67d4cd
redbaron23 <pato>
2020-11-09 10:00:44 -0300

    Todo Item simple

--- client/src/components/ToDoItem.jsx
+++ client/src/components/ToDoItem.jsx
@@ -1,9 +1,45 @@
+import { React, useState } from "react";
+import {
+  makeStyles,
+  Card,
+  Button,
+  CardContent,
+  Checkbox
+} from "@material-ui/core/";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  deatils: {
+    flexDirection: "row",
+    display: "flex"
+  },
+  content: {
+    flex: "1 0 auto"
+  }
+}));
+
 const ToDoItem = props => {
+  const classes = useStyles();
+
+  const { text } = props;
+
+  const [description, setDescription] = useState(text);
+
   return (
     <div>
-    Todos putos
+      <Card className={classes.root} variant="outlined">
+        <div className={classes.details}>
+          <CardContent className={classes.content}>
+            <Checkbox />
+            {description}
+            <Button color="primary">Edit</Button>
+          </CardContent>
+        </div>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
 export default ToDoItem;
