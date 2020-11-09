44239cc60a8949c14d8c1d5b1775388a4027a7a0
redbaron23 <pato>
2020-11-09 13:20:35 -0300

    No duplicates

--- client/src/components/ToDoList.jsx
+++ client/src/components/ToDoList.jsx
@@ -9,20 +9,42 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+
 const ToDoList = () => {
   const classes = useStyles();
 
   const [items, setItems] = useState([]);
 
+
+  const destroy = (item) => {
+    console.log("elemento a eliminar",item)
+    const arr = items.filter(i => i !==item);
+    
+    setItems(arr);
+    console.log("Ahora tengo",arr)
+
+    /*
+    let newItems = [...items];
+
+     newItems = newItems.filter( i => i!==item );
+    console.log("El nuevo es",newItems);
+    setItems(newItems);*/
+  }
+
   const createItem = item => {
-    setItems([...items, item]);
+    const exist = items.filter(i => i===item);
+    (!exist) && setItems([...items, item]);
+    console.log("Ahora tengo",items)
   };
   return (
     <div className={classes.root}>
       <Grid container xs={12} sm={12} md={12} spacing={2}>
-        {items.map(i => (
+
+	{console.log("Voy aponer",items)}
+        {
+	  items.map(i => (
           <Grid item xs={12} sm={6} md={3}>
-            <ToDoItem text={i} />
+            <ToDoItem text={i} destroy={destroy}/>
           </Grid>
         ))}
         <Grid item xs={12}>
