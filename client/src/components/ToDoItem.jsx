f7fd247af1385f0c0c8ab659ba74931811444eaa
redbaron23 <pato>
2020-11-09 12:40:41 -0300

    Edit

--- client/src/components/ToDoItem.jsx
+++ client/src/components/ToDoItem.jsx
@@ -1,5 +1,10 @@
 import { React, useState } from "react";
 import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
   makeStyles,
   Card,
   Button,
@@ -17,24 +22,80 @@ const useStyles = makeStyles(theme => ({
   },
   content: {
     flex: "1 0 auto"
+  },
+  checkbox: {
+    flexGrow: 0.2
+  },
+  button: {
+    flexGrow: 0.8
   }
 }));
 
+const handleNewDescription = (e,setNewDescription) => {
+  const { name, value } = e.target;
+  console.log(value);
+  setNewDescription(value)
+}
+
+const handleSave = (newDescription,setDescription,openDialog) => {
+  setDescription(newDescription);
+  openDialog(false);
+}
+  
+const handleEdit = openDialog => {
+  openDialog(true);
+};
+
+const handleCloseDialog = (setOpenDialog) => {
+  setOpenDialog(false)
+};
 const ToDoItem = props => {
   const classes = useStyles();
 
   const { text } = props;
 
+  const [ newDescription, setNewDescription ] = useState("");
   const [description, setDescription] = useState(text);
+  const [openDialog, setOpenDialog] = useState(false);
 
   return (
     <div>
+      <Dialog open={openDialog} onClose={() => handleCloseDialog(setOpenDialog)}>
+        <DialogTitle id="form-dialog-title">Editing Task "{text}"</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Task Name"
+	    onChange={(e) => handleNewDescription(e,setNewDescription)}
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+	  <Button onClick={() => handleSave(newDescription,setDescription,setOpenDialog)} color="primary">
+            Save
+          </Button>
+          <Button onClick={() => handleCloseDialog(setOpenDialog)} color="primary">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
       <Card className={classes.root} variant="outlined">
         <div className={classes.details}>
           <CardContent className={classes.content}>
-            <Checkbox />
+            <Checkbox className={classes.checkbox} />
             {description}
-            <Button color="primary">Edit</Button>
+            <Button
+              className={classes.button}
+	      onClick={() => handleEdit(setOpenDialog)}
+              color="primary"
+            >
+              Edit
+            </Button>
+            <Button className={classes.button} color="primary">
+              Remove
+            </Button>
           </CardContent>
         </div>
       </Card>
