e4909941a4575cacea0d50b1097476f378aee40a
redbaron23 <pato>
2020-11-09 23:59:35 -0300

    final commit

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -2,27 +2,38 @@ import { React, useState } from "react";
 
 import "../App.css";
 import ToDoList from "../components/ToDoList";
-
+import LogIn from "../components/LoginForm";
 import FolderList from "../components/FolderList";
-
+import  ButtonAppBar  from "../components/ButtonAppBar";
 
 const Main = () => {
   const [view, setView] = useState("folders");
   const [folderName, setFolderName] = useState("");
 
-
-  const enterFolder = (folderName) => {
+  const enterFolder = folderName => {
     setFolderName(folderName);
-    setView("items")
-  }
+    setView("items");
+  };
   const exitFolder = () => {
-    setView("folders")
+    setView("folders");
+  };
+
+  const logOut = () => {
+
+    setView("login");
   }
+
   return (
-    <header className="App-header">
-      {view === "folders" && <FolderList onFolder={enterFolder}/>}
-      {view === "items" && <ToDoList onExit={exitFolder} folderName={folderName}/>}
-    </header>
+    <div  >
+      <ButtonAppBar onLogout={logOut} logged={view!=="login"} />
+      <div>
+        {view === "login" && <LogIn onLogin={exitFolder} />}
+        {view === "folders" && <FolderList onFolder={enterFolder} />}
+        {view === "items" && (
+          <ToDoList onExit={exitFolder} folderName={folderName} />
+        )}
+      </div>
+    </div>
   );
 };
 export default Main;
